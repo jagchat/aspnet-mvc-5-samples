2817ee85999a48f8629344790f202f7cb984bd9c
Jag <jag.chat@gmail.com>
2015-07-12 12:10:37 -0500

    01 Dropdownlist sample added

---
+++ 01-Dropdownlist (binding, fullpostback)/SampleSol/Sample/Controllers/Sample01Controller.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sample.Models;
+
+namespace Sample.Controllers
+{
+    //- How to bind a dropdownlist from a viewmodel
+    //- How to make an item selected in a dropdownlist through viewmodel
+    //- How to do a full postback using dropdownlist (and retain the value selected after postback)
+    //- How to retrieve the dropdownlist value posted through full postback (not through view model) using various ways
+    public class Sample01Controller : Controller
+    {
+        // GET: Sample01
+        public ActionResult Index()
+        {
+            var m = new Sample01ViewModel();
+            return View(m);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string dummy) //due to overload
+        {
+            var m = new Sample01ViewModel();
+            m.SelectedCountry = Request["ddlCountries"];
+            return View(m);
+        }
+
+        //OR
+        //[HttpPost]
+        //public ActionResult Index(FormCollection collection)
+        //{
+        //    var m = new Sample01ViewModel();
+        //    m.SelectedCountry = collection.Get("ddlCountries");
+        //    return View(m);
+        //}
+
+        //OR
+        //[HttpPost]
+        //public ActionResult Index(string ddlCountries)
+        //{
+        //    var m = new Sample01ViewModel();
+        //    m.SelectedCountry = ddlCountries;
+        //    return View(m);
+        //}
+    }
+}
---
+++ 01-Dropdownlist (binding, fullpostback)/SampleSol/Sample/Models/Sample01ViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sample.Models
+{
+    public class Sample01ViewModel
+    {
+        public string SelectedCountry { get; set; }
+
+        public List<Country> Countries
+        {
+            get
+            {
+                return new List<Country>()
+                {
+                    new Country(){Id="USA", Caption = "United States of America"},
+                    new Country(){Id="IN", Caption = "India"},
+                    new Country(){Id="CAN", Caption = "Canada"},
+                };
+            }
+        }
+
+        public IEnumerable<SelectListItem> GetCountriesListItems()
+        {
+            return Countries.Select(c => new SelectListItem() { Text = c.Caption, Value = c.Id, Selected = c.Id == SelectedCountry });
+        }
+    }
+}
